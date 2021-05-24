e604b017ec5cfcf23218abeec61165f98d7b9573
Marek Grota <marekgro@gmail.com>
2021-05-24 20:45:56 +0200

    spring first commit

---
+++ src/main/java/com/example/SpringRepetition/SpringRepetitionApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringRepetition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRepetitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRepetitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringRepetition/SpringRepetitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringRepetition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRepetitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
